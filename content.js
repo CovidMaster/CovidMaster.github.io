c56bb09444acfa29febd13d57391233697399415
arcoh28 <arcoh28@yahoo.com>
2021-03-31 20:35:02 +0200

    Implement value adaption and events
    And date increment and requirement check, make answer order random, replace \n with <br> in content panel and try YT API

--- content.js
+++ content.js
@@ -4,13 +4,26 @@ let content = [
         answers: [
             {
                 answer: "Der Chinavirus gibt es nicht. Das ist nur eine normale Grippewelle. Die kann uns nichts.",
-                link: "https://www.spiegel.de/politik/ausland/nikol-paschinjan-armeniens-ministerpraesident-kuendigt-ruecktritt-an-a-1f5c3483-cee1-4749-8b05-927f0e056287"
+                link: "https://www.spiegel.de/politik/ausland/nikol-paschinjan-armeniens-ministerpraesident-kuendigt-ruecktritt-an-a-1f5c3483-cee1-4749-8b05-927f0e056287",
+                score:
+                    {
+                        economy: "100",
+                        incidence: "5",
+                        satisfaction: "-2"
+                    }
             },
             {
                 answer: "Eine Taskforce aufstellen und das Gesundheitsamt das Umfeld der Person auf weitere angesteckte Personen untersuchen",
-                link: "https://www.spiegel.de/wirtschaft/ever-given-im-suezkanal-aegypten-bereitet-teilentladen-des-containerschiffs-vor-a-abdd9b9c-3e11-4acd-ba34-696eee7390df"
+                link: "https://www.spiegel.de/wirtschaft/ever-given-im-suezkanal-aegypten-bereitet-teilentladen-des-containerschiffs-vor-a-abdd9b9c-3e11-4acd-ba34-696eee7390df",
+                score:
+                    {
+                        economy: "10",
+                        incidence: "2",
+                        satisfaction: "1"
+                    }
             }
-        ]
+        ],
+        days: 5
     },
     {
         question: "Georg Nüßlein hat 660.000€ Provision für die Vermittlung von Masken erhalten\nWie rechtfertigen sie dies?",
@@ -18,24 +31,50 @@ let content = [
         answers: [
             {
                 answer: "Die Journalisten sollte man am Besten wegsperren.",
+                score:
+                    {
+                        economy: "-100",
+                        incidence: "0",
+                        satisfaction: "-2"
+                    }
             },
             {
                 answer: "Wir werden das \"untersuchen\"",
+                score:
+                    {
+                        economy: "-100",
+                        incidence: "0",
+                        satisfaction: "-1"
+                    }
             }
-        ]
+        ],
+        days: 2
     },
     {
         question: "AstraZenica Impfstoff wirkt nur zu 75%",
         ref: 0,
-        require: "01.01.2021",
+        require: "04.04.2020",
         answers: [
             {
                 answer: "Um die Pandemie zu besiegen müssen wir alles nehmen, was uns zur Verfügung steht. 75% sind besser als 0%.",
+                score:
+                    {
+                        economy: "100",
+                        incidence: "-2",
+                        satisfaction: "2"
+                    }
             },
             {
                 answer: "75% sind viel zu wenig. Wir verklagen den Hersteller auf Schadensersatz.",
+                score:
+                    {
+                        economy: "-10",
+                        incidence: "3",
+                        satisfaction: "-2"
+                    }
             }
-        ]
+        ],
+        days: 4
     },
     {
         question: "29 Millionen AstraZeneca Impfdosen wurden in Italien \"gefunden\"",
@@ -44,11 +83,24 @@ let content = [
         answers: [
             {
                 answer: "Nanu?\nDas kann sich niemand erklären",
+                score:
+                    {
+                        economy: "0",
+                        incidence: "-1",
+                        satisfaction: "0"
+                    }
             },
             {
                 answer: "Wir werden untersuchen, ob diese illegal nach Groß Britanien geliefert werden sollten",
+                score:
+                    {
+                        economy: "-100",
+                        incidence: "0",
+                        satisfaction: "1"
+                    }
             }
-        ]
+        ],
+        days: 3
     },
     {
         question: "Ist dein Einkommen nicht ein bisschen zu gering?",
@@ -58,11 +110,24 @@ let content = [
             {
                 answer: "Masken vermitteln und fette Provision kassieren",
                 link: "https://www.mannheim24.de/mannheim/nikolas-loebel-mannheim-corona-masken-affaere-cdu-bundestag-ruecktritt-kretschmann-parteikrise-90230306.html",
+                score:
+                    {
+                        economy: "1000",
+                        incidence: "0",
+                        satisfaction: "-20"
+                    }
             },
             {
                 answer: "Mir sind andere Dinge wichtiger",
+                score:
+                    {
+                        economy: "0",
+                        incidence: "0",
+                        satisfaction: "5"
+                    }
             }
-        ]
+        ],
+        days: 1
     },
     {
         question: "Der Inzidenzwert ist viel zu hoch.\nWas beschließt du?",
@@ -71,9 +136,21 @@ let content = [
         answers: [
             {
                 answer: "Schulen schließen\nAusgangssperre ab 21 Uhr\nTreffen mit maximal einer Person",
+                score:
+                    {
+                        economy: "-1000",
+                        incidence: "-3",
+                        satisfaction: "0"
+                    }
             },
             {
                 answer: "Grenzwert auf das doppelte erhöhen",
+                score:
+                    {
+                        economy: "100",
+                        incidence: "5",
+                        satisfaction: "-10"
+                    }
             }
         ]
     },
@@ -84,10 +161,23 @@ let content = [
         answers: [
             {
                 answer: "Ich verspreche kostenlose Masken ohne dies vorher mit den Apotheken abgesprochen zu haben",
+                score:
+                    {
+                        economy: "0",
+                        incidence: "0",
+                        satisfaction: "10"
+                    }
             },
             {
                 answer: "Um mein Image kann ich mich noch nach der Pandemie kümmern",
+                score:
+                    {
+                        economy: "0",
+                        incidence: "0",
+                        satisfaction: "-10"
+                    }
             }
-        ]
+        ],
+        days: 3
     },
 ];
--- script.js
+++ script.js
@@ -1,48 +1,168 @@
+let currentContent = content[0];
+let rightOrder = true;
+let executedEvents = new Array(content.length).fill(0);
+
+function getDate(germanDateFormat) {
+	let dateParts = germanDateFormat.split(".");
+	return new Date(dateParts[2], dateParts[1]-1, dateParts[0]);
+}
+
+function requirementFulfilled() {
+	if (currentContent.require) {
+		if (currentContent.ref === 0 && getDate($("#date").html()) < getDate(currentContent.require)) {
+			console.log(currentContent.question);
+			return false;
+		}
+	}
+	return true;
+}
+
 function getRandomContent() {
-	return content[Math.floor(Math.random() * content.length)];
+	do {
+		currentContent = content[Math.floor(Math.random() * content.length)];
+	} while ("event" in currentContent || !requirementFulfilled(currentContent));
+}
+
+function getValue(num) {
+	let value = $("#bar" + num).html();
+	return parseInt(value.replace(/\.|€|%/g, ""));
+}
+
+function checkIfEvent() {
+	for (let i = 0; i < content.length; i++) {
+		if ("event" in content[i] && executedEvents[i] === 0) {
+			for (let j = 0; j < 3; j++) {
+				if (content[i].event === j && (getValue(j+1) <= content[i].max || getValue(j+1) >= content[i].min)) {
+					currentContent = content[i];
+					executedEvents[i] = 1;
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
 
 function setContent() {
-	let newContent = getRandomContent();
-	$("#question").html(newContent.question);
+	if (!checkIfEvent()) {
+		getRandomContent();
+	}
+	let newContent = currentContent;
+	
+	$("#question").html(newContent.question.replace(/\n/g, "<br>"));
 	$("#questionLink").addClass("d-none");
-	if(newContent.link) {
+	if (newContent.link) {
 		$("#questionLink").attr("href", newContent.link);
 		$("#questionLink").removeClass("d-none");
 	}
-	
-	//ToDo: convert the following code into a for loop
-	$("#answer1").html(newContent.answers[0].answer);
-	$("#answer1Link").addClass("d-none");
-	if(newContent.answers[0].link) {
-		$("#answer1Link").removeClass("d-none");
-		$("#answer1Link").attr("href", newContent.answers[0].link);
+
+	rightOrder = true;
+	let answerOrder = [Math.floor(Math.random() * 2 + 1), 2]
+	if (answerOrder[0] === 2) {
+		answerOrder[1] = 1;
+		rightOrder = false;
 	}
-	$("#answer2").html(newContent.answers[1].answer);
-	$("#answer2Link").addClass("d-none");
-	if(newContent.answers[1].link) {
-		$("#answer2Link").removeClass("d-none");
-		$("#answer2Link").attr("href", newContent.answers[1].link);
+	for (let i = 0; i < 2; i++) {
+		$("#answer" + answerOrder[i]).html(newContent.answers[i].answer.replace(/\n/g, "<br>"));
+		$("#answer" + answerOrder[i] + "Link").addClass("d-none");
+		if (newContent.answers[i].link) {
+			$("#answer" + answerOrder[i] + "Link").removeClass("d-none");
+			$("#answer" + answerOrder[i] + "Link").attr("href", newContent.answers[i].link);
+		}
 	}
+}
+
+function increaseDate() {
+	let date = getDate($("#date").html());
+	date.setDate(date.getDate() + 1);
+	let day = date.getDate();
+    let month = date.getMonth() + 1;
+    if ((String(day)).length === 1) {
+    	day ="0" + day;
+	}
+    if ((String(month)).length === 1) {
+    	month = "0" + month;
+	}
+	$("#date").html(day + "." + month + "." + date.getFullYear());
+}
+
+function changeScore(clickedAnswer) {
+	const baseScore = [10, 1, 1];
+	let score = currentContent.answers[0].score;
+	if ((!rightOrder && clickedAnswer === "answer1Parent") || (rightOrder && clickedAnswer !== "answer1Parent")) {
+		score = currentContent.answers[1].score;
+	}
+
+	let value1 = getValue(1) + baseScore[0] + parseInt(score.economy);
+	if (value1 > 50000) {
+		value1 = 50000;
+	} else if (value1 < 0) {
+		value1 = 0;
+	}
+	$("#bar1").html(value1.toFixed(0).replace(".", ",").replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.") + "€");
+	width1 = value1 / 50000 * 100 + "%";
+	$("#bar1").css('width', width1);
+
+	let value2 = getValue(2) + baseScore[1] + parseInt(score.incidence);
+	if (value2 > 500) {
+		value2 = 500;
+	} else if (value2 < 0) {
+		value2 = 0;
+	}
+	$("#bar2").html(value2);
+	width2 = value2 / 500 * 100 + "%";
+	$("#bar2").css('width', width2);
+
+	let value3 = getValue(3) + baseScore[2] + parseInt(score.satisfaction);
+	if (value3 > 100) {
+		value3 = 100;
+	} else if (value3 < 0) {
+		value3 = 0;
+	}
+	$("#bar3").html(value3 + "%");
+	width3 = value3 + "%";
+	$("#bar3").css('width', width3);
 }
 
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function displayAnswers() {
-	await sleep(1000);
+async function answerImpact(clickedAnswer) {
+	let days = content[0].days;
+	if (currentContent.days) {
+		days = currentContent.days;
+	}
+	for (let i = 0; i < days; i++) {
+		await sleep(30);
+		increaseDate();
+		changeScore(clickedAnswer);
+	}
 	setContent();
 	$("#panel").css("display", "block");
 }
 
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player("backgroundMusic", {
+		events: {
+			"onReady": onPlayerReady
+		}
+	});
+}
+
+function onPlayerReady(event) {
+	event.target.playVideo();
+  }
+
 function init() {
 	setContent();
 	$(".answerParent").click(function() {
-		//console.log($( this ).html());
 		$("#panel").css("display", "none");
-		displayAnswers();
-	})
+		answerImpact($( this ).attr("id"));
+	});
+	//player.setVolume(25);
+	//player.playVideo();
 }
 
 $(function() {
